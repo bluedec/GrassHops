4162d8a7a50d2d3d24d9974c585cb14375f4ff69
bluedec <yisussaez@gmail.com>
2023-07-17 22:54:33 -0300

    Added animation player

--- scripts/johnson.gd
+++ scripts/johnson.gd
@@ -1,7 +1,8 @@
 extends CharacterBody2D
 
 @onready var speed = 120
-@onready var anim = $AnimatedSprite2D
+@onready var sprite = $AnimatedSprite2D
+@onready var anim = $AnimatedSprite2D/AnimationPlayer
 var combo : int = 0
 var combo_cooldown : float = 0.5
 
@@ -13,20 +14,24 @@ func _physics_process(_delta):
 	input_direction.normalized()
 	
 	if input_direction.x > 0:
-		anim.flip_h = false
+		sprite.flip_h = false
+		sprite.animation = "run"
 		anim.play("run")
 		calculate_velocity_and_slide(input_direction)
 		return
 	elif input_direction.x < 0:
-		anim.flip_h = true
+		sprite.flip_h = true
+		sprite.animation = "run"
 		anim.play("run")
 		calculate_velocity_and_slide(input_direction)
 		return
 	elif input_direction.y > 0 or input_direction.y < 0:
+		sprite.animation = "run"
 		anim.play("run")
 		calculate_velocity_and_slide(input_direction)
 		return
 	else:
+		sprite.animation = "idle"
 		anim.play("idle")
 		
 	combo_cooldown -= 0.01
@@ -37,6 +42,7 @@ func _physics_process(_delta):
 	
 func attack(combo):
 	if combo == 0:
+		sprite.animation = "punch"
 		anim.play("punch")
 		combo += 1
 	elif combo == 1:
@@ -54,5 +60,3 @@ func calculate_velocity_and_slide(input_direction):
 	velocity = input_direction * speed
 	move_and_slide()
 	
-func _on_punch_animation_finished():
-	print("Animation finished.")
