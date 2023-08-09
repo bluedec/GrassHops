19a715c65b9af72f28a07ea9e42a4507de1ac0d8
bluedec <yisussaez@gmail.com>
2023-08-09 00:07:28 -0300

    Minor Character change

--- scenes/characters/Sonny/Sonny.gd
+++ scenes/characters/Sonny/Sonny.gd
@@ -7,12 +7,17 @@ extends CharacterBody2D
 
 var combo : int = 0
 var combo_cooldown : float = 0.5
-var dir = "down"
-var dmg : int = 49
-var health : int = 295
-var knockback_strength := Vector2()
+var dir = 2
+@export var dmg : int = 49
+@export var health : int = 295
+@export var knockback_strength := Vector2()
+var breath_speed_slowed := 0.3
 
 func _ready():
+	var timer = Timer.new()
+	timer.wait_time = 5
+	
+	
 	pass
 
 func _physics_process(delta):
@@ -41,25 +46,25 @@ func _physics_process(delta):
 	if is_attacking():
 		return
 		
-	if dir == "left":
+	if dir == 3:
 		if moving:
 			anim.play("left_side_running")
 		else:
 			anim.play("left_side_idle")
 
-	elif dir == "right":
+	elif dir == 1:
 		if moving:
 			anim.play("right_side_running")
 		else:
 			anim.play("right_side_idle")
 #
-	elif dir == "down":
+	elif dir == 2:
 		if moving:
 			anim.play("running_front")
 		else:
 			anim.play("idle")
 			
-	elif dir == "up":
+	elif dir == 0:
 		if moving:
 			anim.play("running_upwards")
 		else:
@@ -67,22 +72,27 @@ func _physics_process(delta):
 
 	combo_cooldown -= 0.01
 
+func lower_breath_speed():
+	if anim.current_animation == "idle" \
+	or anim.current_animation == "left_side_idle":
+		anim.speed_scale = breath_speed_slowed
+	pass
 
 func handle_movement(input_direction):
 	if input_direction.x > 0:
-		dir = "right"
+		dir = 1
 		calculate_velocity_and_slide(input_direction)
 		
 	elif input_direction.x < 0:
-		dir = "left"
+		dir = 3
 		calculate_velocity_and_slide(input_direction)
 		
 	elif input_direction.y > 0:
-		dir = "down"
+		dir = 2
 		calculate_velocity_and_slide(input_direction)
 		
 	elif input_direction.y < 0:
-		dir = "up"
+		dir = 0
 		calculate_velocity_and_slide(input_direction)
 		
 		
@@ -93,25 +103,25 @@ func is_attacking():
 	or anim.current_animation == "attack_up_1"
 	
 func attack():
-	if dir == "down":
+	if dir == 2:
 		anim.play("attack_down")
 		$Sword_Hitbox_Detector/Sword_Hitbox_Downwards.disabled = false
-	elif dir == "left":
+	elif dir == 3:
 		anim.play("left_side_attack")
 		$Sword_Hitbox_Detector/Sword_Hitbox_Left.disabled = false
-	elif dir == "right":
+	elif dir == 1:
 		anim.play("right_side_attack")
 		$Sword_Hitbox_Detector/Sword_Hitbox_Right.disabled = false
-	elif dir == "up":
+	elif dir == 0:
 		anim.play("attack_up_1")
 		$Sword_Hitbox_Detector/Sword_Hitbox_Up.disabled = false
 
 #	if combo == 0:
-#		if dir == "down":
+#		if dir == 2:
 #			anim.play("attack_down")
-#		elif dir == "left":
+#		elif dir == 3:
 #			anim.play("left_side_attack")
-#		elif dir == "right":
+#		elif dir == 1:
 #			anim.play("right_side_attack")
 #		combo += 1
 #	elif combo == 1:
@@ -126,13 +136,13 @@ func attack():
 
 
 func attack_thrust(dir):
-	if dir == "right":
+	if dir == 1:
 		position.x += 4
-	elif dir == "left":
+	elif dir == 3:
 		position.x -= 4
-	elif dir == "up":
+	elif dir == 0:
 		position.y -= 4
-	elif dir == "down":
+	elif dir == 2:
 		position.y += 4
 	
 
@@ -149,13 +159,13 @@ func take_damage(dmg):
 func _on_sword_hitbox_detector_area_entered(area):
 	var parent = area.get_parent()
 	
-	if position.x > parent.position.x && dir == "left":
+	if position.x > parent.position.x && dir == 3:
 		knockback_strength.x = -200
-	elif position.x < parent.position.x && dir == "right":
+	elif position.x < parent.position.x && dir == 1:
 		knockback_strength.x = 200
-	if position.y > parent.position.y && dir == "up":
+	if position.y > parent.position.y && dir == 0:
 		knockback_strength.y = -200
-	elif position.y < parent.position.y && dir == "down":
+	elif position.y < parent.position.y && dir == 2:
 		knockback_strength.y = 200
 		
 	parent.take_damage(dmg, knockback_strength)
